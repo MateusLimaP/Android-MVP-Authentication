aad4d5a1f6544536b3eb7efdba560dd07d610e26
MateusLimaP <mateuslimamtc@gmail.com>
2021-06-07 12:52:53 -0300

    version database increased

--- app/src/main/java/com/mateuslima/mvpcats/data/db/UserDatabase.java
+++ app/src/main/java/com/mateuslima/mvpcats/data/db/UserDatabase.java
@@ -2,14 +2,17 @@ package com.mateuslima.mvpcats.data.db;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.mateuslima.mvpcats.data.db.dao.UserDao;
 import com.mateuslima.mvpcats.data.db.model.User;
 
-@Database(entities = User.class, version = 1)
+@Database(entities = User.class, version = 2)
 public abstract class UserDatabase extends RoomDatabase {
 
     public abstract UserDao userDao();
@@ -20,10 +23,17 @@ public abstract class UserDatabase extends RoomDatabase {
         if (instance == null){
             instance = Room.databaseBuilder(context.getApplicationContext(), UserDatabase.class, "user_database")
                     .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
+                    .addMigrations(MIGRATION_1_2)
                     .build();
         }
         return instance;
     }
+
+    private static final Migration MIGRATION_1_2 = new Migration(1,2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+
+        }
+    };
 
 }
